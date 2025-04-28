faa75695389a597cc29ad070e12b12a9b745a512
Steffin12-git <steffinthomas12@gmail.com>
2025-04-28 09:11:30 +0530

    feat: Add EDA queries for `Airport Performance`

--- Fligh delay/Flight delay EDA.sql
+++ Fligh delay/Flight delay EDA.sql
@@ -2,13 +2,16 @@
 USE [flight-delays]
 GO
 
+---------------------------------------------------------------------------------------------------------------------------------------
 
 -- Airline Performance:
 	-- What is the average delay per airline?
 	-- Which airline has the highest number of delayed flights?
 	-- Which airline has the most on-time flights?
 
+
 -- What is the average delay per airline?
+
 SELECT TOP 1000 * 
 FROM dbo.flights
 
@@ -26,7 +29,10 @@ LEFT JOIN
 GROUP BY AL.AIRLINE
 ORDER BY AVG_TIME DESC;
 
+
+
 -- Which airline has the highest number of delayed flights?
+
 SELECT TOP 1
 	AL.AIRLINE AIRLINE,
 	COUNT(*) AS Total_Flights,
@@ -37,6 +43,8 @@ LEFT JOIN
 			AL.IATA_CODE = FL.AIRLINE
 GROUP BY AL.AIRLINE
 ORDER BY DELAYED_FLIGHTS DESC;
+
+
 
 -- Which airline has the most on-time flights?
 SELECT TOP 1
@@ -49,3 +57,40 @@ LEFT JOIN
 			AL.IATA_CODE = FL.AIRLINE
 GROUP BY AL.AIRLINE
 ORDER BY NO_DELAYED_FLIGHTS DESC;
+
+
+---------------------------------------------------------------------------------------------------------------------------------------
+-- Airport Performance:
+	-- Which airport has the highest departure delays?
+	-- Which airport has the most number of delayed arrivals?
+
+
+-- Which airport has the highest departure delays?
+SELECT TOP 1
+	FL.ORIGIN_AIRPORT AIRPORT_CODE,
+	AP.AIRPORT AIRPORT,
+	COUNT(FL.ORIGIN_AIRPORT) AS Total_Flights,
+	AVG(FL.DEPARTURE_DELAY ) AS DELAYED_FLIGHTS_AVG
+FROM dbo.airports AP
+JOIN  
+	dbo.flights FL ON
+			AP.IATA_CODE = FL.ORIGIN_AIRPORT
+WHERE FL.DEPARTURE_DELAY IS NOT NULL
+GROUP BY FL.ORIGIN_AIRPORT, AP.AIRPORT
+ORDER BY DELAYED_FLIGHTS_AVG DESC;
+
+
+-- Which airport has the most number of delayed arrivals?
+SELECT TOP 1
+	FL.DESTINATION_AIRPORT AIRPORT_CODE,
+	AP.AIRPORT AIRPORT,
+	COUNT(FL.DESTINATION_AIRPORT) AS Total_Flights,
+	COUNT(CASE WHEN ARRIVAL_DELAY > 0 THEN 1 END) AS DELAYED_FLIGHTS_AVG
+FROM dbo.airports AP
+JOIN  
+	dbo.flights FL ON
+			AP.IATA_CODE = FL.DESTINATION_AIRPORT
+GROUP BY FL.DESTINATION_AIRPORT, AP.AIRPORT
+ORDER BY DELAYED_FLIGHTS_AVG DESC;
+
+---------------------------------------------------------------------------------------------------------------------------------------
